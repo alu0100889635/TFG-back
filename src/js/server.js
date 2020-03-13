9ee1c02f428f2c6e8d31214158ce6f8a83e9383e
Alba Cruz Torres <alu0100889635@ull.edu.es>
2020-03-13 09:53:12 +0000

    Se crean los archivos con las rutas y se añaden al servidor

--- src/js/server.js
+++ src/js/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const routerInfo = require("../router/routerInfo");
+const routerPasswd = require("../router/routerPasswd");
+
 app.use(express.json());
+app.use("/info", routerInfo);
+app.use("/passwd", routerPasswd);
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
