d000934b5143045968c905432bb2ab5d1b1328d6
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-16 17:28:08 +0100

    Se añade saltGenerator

--- src/routers/subjectRouter.js
+++ src/routers/subjectRouter.js
@@ -6,7 +6,6 @@ router.get("/", subjectController.showAll);
 router.get("/:id", subjectController.showSubject);
 router.post("/addSubject", subjectController.addSubject);
 router.delete("/:id", subjectController.deleteSubject);
-router.delete("/deleteAll", subjectController.deleteAll);
 router.put("/:id", subjectController.updateSubject);
 
 
