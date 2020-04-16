641bc40477a93d25a1fcd816c5dc4e03f79b1e1c
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-16 13:48:47 +0100

    Se prueba una posible solución al borrado de esas keys

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -3,14 +3,15 @@ const Subject = require('../models/Subject');
 async function showAll(req, res) {
 	let subjects = await Subject.find();
 	for(let i = 0; i < subjects.length; i++){
-		subjects[i] = {
+		subjects[i] = _.omit(subjects[i], ['__enc_fullName','__enc_dni', '__enc_birthDate_d', '__enc_birthDate', '__enc_address']);
+		/* subjects[i] = {
 			id_: subjects[i].id_,
 			fullName: subjects[i].fullName,
 			dni: subjects[i].dni,
 			birthDate: subjects[i].birthDate,
 			address: subjects[i].address,
 			__v: subjects[i].__v
-		}
+		} */
 	}
 	res.json(subjects);
 }
