fab4fac3560a5a2b55f08c04b5d1e00c723b34bf
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-16 12:17:07 +0100

    Se añade otra implementación usando getters y setters para la encriptación

--- src/models/Subject.js
+++ src/models/Subject.js
@@ -1,23 +1,47 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require("crypto");
+
+const encrypt = (text) => {
+	let cipher = crypto.createCipher('aes-256-cbc', process.env.SERVER_SECRET);
+  	let crypted = cipher.update(text,'utf8','hex');
+  	crypted += cipher.final('hex');
+  	return crypted;
+}
+
+const decrypt = (text) => {
+	if (text === null || typeof text === 'undefined') {return text;};
+  	let decipher = crypto.createDecipher('aes-256-cbc', process.env.SERVER_SECRET);
+  	let dec = decipher.update(text,'hex','utf8');
+  	dec += decipher.final('utf8');
+  	return dec;
+}
 
 const SubjectSchema = new Schema({
 	fullName: {
 		type: String,
-		required: true
+		required: true,
+		get: decrypt,
+		set: encrypt
 	},
 	dni: {
 		type: String,
 		unique: true,
-		required: true
+		required: true,
+		get: decrypt,
+		set: encrypt
 	},
 	birthDate: {
 		type: Date,
-		required: true
+		required: true,
+		get: decrypt,
+		set: encrypt
 	},
 	address:{
 		type: String,
-		required: true
+		required: true,
+		get: decrypt,
+		set: encrypt
 	},
     createdAt:{
         type: Date
