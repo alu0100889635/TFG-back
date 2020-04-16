686f8c1a657e0fbf45d9002eff763ead57627ab4
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-16 13:23:19 +0100

    Se modifica subjects para que no muestre los __enc_...

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -2,11 +2,12 @@ const Subject = require('../models/Subject');
 
 async function showAll(req, res) {
 	let subjects = await Subject.find();
+	subjects.forEach(element => ['one', 'three', 'five'].forEach(function (k) {
+		delete element[k];
+	}));
 	console.log(subjects);
-	/* for(let i = 0; i < subjects.length; i++){
-		['__enc_fullName','__enc_dni', '__enc_birthDate_d', '__enc_birthDate', '__enc_address'].forEach(Reflect.deleteProperty.bind(null, subjects[i]));
-	}
-	res.json(subjects); */
+	
+	res.json(subjects);
 }
 
 async function showSubject(req, res) {
