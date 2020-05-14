4eb60353374f176a6edb15977da108806c403771
Alba Cruz Torres <albacruztorres@gmail.com>
2020-05-14 14:08:09 +0100

    Se sigue sin saber el error

--- src/controllers/phonecallController.js
+++ src/controllers/phonecallController.js
@@ -9,7 +9,7 @@ async function showFirstPhonecall(req, res){
 	const foundPhonecall = await Phonecall.find();
 	console.log(foundPhonecall);
 	console.log(typeof foundPhonecall);
-	res.json(foundPhonecall[0]);
+	//res.json(foundPhonecall[0]);
 }
 
 async function showPhonecall(req, res){
